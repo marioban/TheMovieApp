baf417a6a512ed4979789ab9d666c07c98baa3b9
marioban <marioban00@gmail.com>
2025-01-28 08:38:04 +0100

    initial commit

---
+++ TheMovieApp/TheMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieAppApp.swift
+//  TheMovieApp
+//
+//  Created by Mario Ban on 28.01.2025..
+//
+
+import SwiftUI
+
+@main
+struct TheMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
