c05409a90e8e538ce600059a83dabb80e00b1a81
marioban <marioban00@gmail.com>
2025-01-28 10:20:43 +0100

    setup

---
+++ TheMovieApp/Root/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  TheMovieApp
+//
+//  Created by Mario Ban on 28.01.2025..
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
